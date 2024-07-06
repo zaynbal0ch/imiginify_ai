8f896ad058c28effb4a6dc70010ad0a3b092472e
Zayn Baloch <zainurrehman005@gmail.com>
2024-07-07 04:16:25 +0500

    Layout Sider & Mobile Navigation

---
+++ components/shared/Sidebar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { navLinks } from '@/Constants'
+import { SignedIn } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const Sidebar = () => {
+    const pathname = usePathname();
+
+  return (
+    <aside className="sidebar">
+        <div className="flex size-full flex-col gap-4">
+            <Link href="/" className="sidebar-logo">
+                <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
+            </Link>
+
+            <nav className="sidebar-nav">
+                <SignedIn>
+                    <ul className="sidebar-nav_elements">
+                        {navLinks.map((link) =>{
+                            const isActive = link.route === pathname
+
+                            return (
+                                <li key={link.route} className={`sidebar-nav_elements group ${
+                                    isActive ? 'bg-purpule-gradient text-white' : 'text-gray-700'
+                                }`}>
+                                  {link.label}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </SignedIn>
+            </nav>
+        </div>
+    </aside>
+  )
+}
+
+export default Sidebar
